88a0bc312d6911900f1d89c1cd35767556cdc3c1
bwnstk <info@flimmerkiste.xyz>
2020-10-05 15:56:48 +0200

    Added Logo and LogoText

---
+++ src/components/logo/logo.stories.js
@@ -0,0 +1,8 @@
+import "./logo.css";
+import logoText from "./logo-text.html";
+import logo from "./logo.html";
+
+export default { title: "Components/Logo" };
+
+export const logoOriginal = () => logo;
+export const logoWithText = () => logoText;
