f60e19d1c7ac1f9cd83614dccc7b4940ffebb087
bwnstk <info@flimmerkiste.xyz>
2020-10-06 16:19:51 +0200

    Add PlayerControls

--- src/components/musicControls/player.stories.js
+++ src/components/musicControls/player.stories.js
@@ -1,9 +1,30 @@
 import "./player.css";
 
 import playerBack from "./back.html";
-import playerAll from "./player-all.html";
+import playerPlay from "./play.html";
+import playerNext from "./next.html";
+import playerPlaylist from "./playlist.html";
+import playerRepeat from "./repeat.html";
+import playerEq from "./eq.html";
+import playerShuffle from "./shuffle.html";
+import playerFav from "./favorites.html";
+
+import playerControls from "./player-controls.html";
+import playerMenu from "./player-menu.html";
+import player from "./player.html";
 
 export default { title: "Components/PlayerControls" };
 
 export const back = () => playerBack;
-export const allControls = () => playerAll;
+export const play = () => playerPlay;
+export const next = () => playerNext;
+
+export const playlist = () => playerPlaylist;
+export const repeat = () => playerRepeat;
+export const equalizer = () => playerEq;
+export const shuffle = () => playerShuffle;
+export const favorites = () => playerFav;
+
+export const allControls = () => playerControls;
+export const menu = () => playerMenu;
+export const wholePlayer = () => player;
