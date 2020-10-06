e5ce0f0065420590715960c2420befbe1b1a6975
bwnstk <info@flimmerkiste.xyz>
2020-10-06 14:43:19 +0200

    Add filestructure for Player Buttons

---
+++ src/components/musicControls/player.stories.js
@@ -0,0 +1,9 @@
+import "./player.css";
+
+import playerBack from "./back.html";
+import playerAll from "./player-all.html";
+
+export default { title: "Components/PlayerControls" };
+
+export const back = () => playerBack;
+export const allControls = () => playerAll;
