25c149aea574fbea78c038ffbe661f5f5d1059ce
bwnstk <info@flimmerkiste.xyz>
2020-10-07 15:18:39 +0200

    Add Navigation with Songs

--- src/pages/navigation/nav.stories.js
+++ src/pages/navigation/nav.stories.js
@@ -1,6 +1,8 @@
 import "./nav.css";
 import nav from "./nav.html";
+import navSongs from "./navSongs.html";
 
 export default { title: "Pages/Navigation" };
 
 export const buttonNavigation = () => nav;
+export const navWithSongs = () => navSongs;
